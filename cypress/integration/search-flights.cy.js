f21d1358fbc406c37d1f83982756468097f9d7eb
cristina-iordanescu <cristinaiordanescu978@gmail.com>
2022-07-10 16:02:16 +0300

    made some tweaks

--- cypress/integration/search-flights.cy.js
+++ cypress/integration/search-flights.cy.js
@@ -5,12 +5,13 @@ import HomePage_PO from "../support/pageObjects/HomePage_PO";
 describe("Search for flight", () => {
     const homepage = new HomePage_PO();
     beforeEach(function () {
-        cy.visit('/')
-        cy.get('[data-test="CookiesPopup-Accept"]').click()
+        cy.visit(Cypress.env('homepage'))
+        homepage.acceptCookies();
+        homepage.uncheckBooking()
     });
 
     it("Search for a flight with specific arrival/departure time", () => {
-        let destination = Cypress.env('destination');
+        let destination = 'Barcelona';
 
         homepage.enterDestination(destination)
         homepage.selectOutboundDate()
